59ceb72165dbdd4d4e16113edf1db42a42b34e44
nafisa <isajd@ojdsa>
2024-04-03 13:55:46 -0400

    Added DrawPanel, Player, and Monster class (Rat class is an extension.) Added instance variables to parent Monster class.

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,2 +1,13 @@
-public class DrawPanel {
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.Font;
+
+public class DrawPanel extends JPanel implements MouseListener {
+
+
 }
