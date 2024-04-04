522b4e258b8e660beb514a9b051ad18da0826428
nafisa <sajdi@djaoid>
2024-04-04 13:00:09 -0400

    - added methods to WorldFrame class

--- src/WorldFrame.java
+++ src/WorldFrame.java
@@ -1,2 +1,29 @@
-public class WorldFrame {
+import javax.swing.JFrame;
+public class WorldFrame extends JFrame implements Runnable {
+    private WorldPanel p;
+    private Thread windowThread;
+
+    public WorldFrame(String display){
+        super(display);
+        int frameWidth = 800;
+        int frameHeight = 800;
+        p = new WorldPanel();
+        this.add(p);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(frameWidth, frameHeight);
+        this.setLocation(600, 100);
+        this.setVisible(true);
+        startThread();
+    }
+
+    public void startThread() {
+        windowThread = new Thread(this);
+        windowThread.start();
+    }
+
+    public void run(){
+        while (true){
+            p.repaint();
+        }
+    }
 }
