24982b954c1731d3765f8fb8a9d44e7f14f81eb6
veronica <vadsfaisfd@gmail.com>
2024-04-04 13:12:41 -0400

    - added getter and setter methods in player class

--- src/Player.java
+++ src/Player.java
@@ -28,5 +28,23 @@ public class Player {
         }
     }
 
+    public BufferedImage getImage() {
+        return image;
+    }
 
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
