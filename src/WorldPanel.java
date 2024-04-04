9e67fa815eb5e9f51e79001fe72abd335af0f8aa
nafisa <sajdi@djaoid>
2024-04-04 13:05:44 -0400

    - testing displaying window worldPanel

--- src/WorldPanel.java
+++ src/WorldPanel.java
@@ -1,12 +1,34 @@
-import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.awt.event.KeyEvent;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.Font;
 
 public class WorldPanel extends JPanel implements MouseListener {
+
+    private Rectangle button;
+
+    public WorldPanel(){
+        button = new Rectangle(75, 200, 160, 26);
+        this.addMouseListener(this);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawRect((int) button.getX(), (int) button.getY(), (int) button.getWidth(), (int) button.getHeight());
+    }
+
+    public void mousePressed(MouseEvent e){
+        Point clicked = e.getPoint();
+    }
+
+    public void mouseReleased(MouseEvent e) { }
+    public void mouseEntered(MouseEvent e) { }
+    public void mouseExited(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) { }
 
 
 }
