0cfeacbf8157f4d690521e578c00c6ebb1639ced
veronica <vadsfaisfd@gmail.com>
2024-04-04 13:03:32 -0400

    - added player class and sprite folder

---
+++ src/Player.java
@@ -0,0 +1,32 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class Player {
+    private BufferedImage image;
+    private int row;
+    private int col;
+    private final String image_file = "sprites/player_file_cat.png";
+
+    public Player(int row, int col) {
+        this.row = row;
+        this.col = col;
+        image = loadImage(image_file);
+    }
+
+    public BufferedImage loadImage(String fileName) {
+        try {
+            BufferedImage image;
+            image = ImageIO.read(new File(fileName));
+            return image;
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+}
