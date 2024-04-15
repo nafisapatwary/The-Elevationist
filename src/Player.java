ebe20598b88e52e7eb84a1c1c6eb799a68372f57
nafisa <sadj@jdas>
2024-04-15 13:09:57 -0400

    Added rectangle to player class

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,19 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.lang.Object;
+
 
 public class Player {
     private BufferedImage image;
 
     private final String image_file = "sprites/player_file_cat.png";
+    private int x = 502;
+    private int y = 1000;
+    private Rectangle playerRect = new Rectangle(47, 47, x, y);
 
     public Player() {
         image = loadImage(image_file);
@@ -29,4 +35,27 @@ public class Player {
         return image;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public Rectangle getPlayerRect() {
+        return playerRect;
+    }
+
+    public void updateRectPos() {
+        this.playerRect = new Rectangle(47, 47, x, y);
+    }
 }
